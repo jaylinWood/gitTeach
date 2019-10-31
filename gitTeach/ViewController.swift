75a42e20e6f36476eec8a54772232f01a53b2c28
getHub <getHub>
2019-10-31 13:43:57 -0400

    Initial Commit

---
+++ gitTeach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTeach
+//
+//  Created by jaylin wood on 10/31/19.
+//  Copyright © 2019 jaylin wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
